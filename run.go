299c7fa2fd16fa356355bf97072e82f097a8fbdd
John Wregglesworth <wregglej@iplantcollaborative.org>
2017-01-13 13:07:46 -0700

    Move logic to create the logs directory

--- run.go
+++ run.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"path"
 	"strings"
 	"time"
 
@@ -338,20 +337,6 @@ func Run(client *messaging.Client, dckr *dockerops.Docker, exit chan messaging.S
 	// // Create the working directory volume
 	if runner.status == messaging.Success {
 		if _, err = runner.dckr.CreateWorkingDirVolume(job.InvocationID); err != nil {
-			logcabin.Error.Print(err)
-		}
-
-		voldir := path.Join(dockerops.VOLUMEDIR, "logs")
-		err = os.Mkdir(voldir, 0755)
-		if err != nil {
-			logcabin.Error.Print(err)
-		}
-
-		if err = writeJobSummary(voldir, job); err != nil {
-			logcabin.Error.Print(err)
-		}
-
-		if err = writeJobParameters(voldir, job); err != nil {
 			logcabin.Error.Print(err)
 		}
 	}
