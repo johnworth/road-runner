777737f4a1d210987c0be3e80fbecc031c435869
John Wregglesworth <wregglej@iplantcollaborative.org>
2017-03-30 14:01:50 -0700

    exit.go uses github.com/pkg/errors

--- exit.go
+++ exit.go
@@ -7,115 +7,130 @@ import (
 	"github.com/cyverse-de/logcabin"
 	"github.com/cyverse-de/messaging"
 	"github.com/cyverse-de/model"
+	"github.com/pkg/errors"
 )
 
 // RemoveVolume removes the docker volume with the provided volume identifier.
-func RemoveVolume(id string) {
+func RemoveVolume(id string) error {
 	var (
 		err       error
 		hasVolume bool
 	)
 	hasVolume, err = dckr.VolumeExists(id)
 	if err != nil {
-		logcabin.Error.Print(err)
+		return errors.Wrap(err, "failed to check for volume existence")
 	}
 	if hasVolume {
 		logcabin.Info.Printf("removing volume: %s", id)
 		if err = dckr.RemoveVolume(id); err != nil {
-			logcabin.Error.Print(err)
+			return errors.Wrap(err, "failed to remove volume")
 		}
 	}
+	return nil
 }
 
 // RemoveJobContainers removes containers based on their job label.
-func RemoveJobContainers(id string) {
+func RemoveJobContainers(id string) error {
 	logcabin.Info.Printf("Finding all containers with the label %s=%s", model.DockerLabelKey, id)
 	jobContainers, err := dckr.ContainersWithLabel(model.DockerLabelKey, id, true)
 	if err != nil {
-		logcabin.Error.Print(err)
-		jobContainers = []string{}
+		return errors.Wrapf(err, "failed to find containers with label %s=%s", model.DockerLabelKey, id)
 	}
 	for _, jc := range jobContainers {
 		logcabin.Info.Printf("Nuking container %s", jc)
 		err = dckr.NukeContainer(jc)
 		if err != nil {
-			logcabin.Error.Print(err)
+			return errors.Wrapf(err, "failed to remove container %s", jc)
 		}
 	}
+	return nil
 }
 
 // RemoveDataContainers attempts to remove all data containers.
-func RemoveDataContainers() {
+func RemoveDataContainers() error {
 	logcabin.Info.Println("Finding all data containers")
 	dataContainers, err := dckr.ContainersWithLabel(dockerops.TypeLabel, strconv.Itoa(dockerops.DataContainer), true)
 	if err != nil {
-		logcabin.Error.Print(err)
+		return errors.Wrapf(err, "failed to find containers with label %s=%s", dockerops.TypeLabel, strconv.Itoa(dockerops.DataContainer))
 	}
 	for _, dc := range dataContainers {
 		logcabin.Info.Printf("Nuking data container %s", dc)
 		err = dckr.NukeContainer(dc)
 		if err != nil {
-			logcabin.Error.Print(err)
+			return errors.Wrapf(err, "failed to remove container %s", dc)
 		}
 	}
+	return nil
 }
 
 // RemoveStepContainers attempts to remove all step containers.
-func RemoveStepContainers() {
+func RemoveStepContainers() error {
 	logcabin.Info.Println("Finding all step containers")
 	stepContainers, err := dckr.ContainersWithLabel(dockerops.TypeLabel, strconv.Itoa(dockerops.StepContainer), true)
 	if err != nil {
-		logcabin.Error.Print(err)
+		return errors.Wrapf(err, "failed to find containers with label %s=%s", dockerops.TypeLabel, strconv.Itoa(dockerops.StepContainer))
 	}
 	for _, sc := range stepContainers {
 		logcabin.Info.Printf("Nuking step container %s", sc)
 		err = dckr.NukeContainer(sc)
 		if err != nil {
-			logcabin.Error.Print(err)
+			return errors.Wrapf(err, "failed to remove container %s", sc)
 		}
 	}
+	return nil
 }
 
 // RemoveInputContainers attempts to remove all input containers.
-func RemoveInputContainers() {
+func RemoveInputContainers() error {
 	logcabin.Info.Println("Finding all input containers")
 	inputContainers, err := dckr.ContainersWithLabel(dockerops.TypeLabel, strconv.Itoa(dockerops.InputContainer), true)
 	if err != nil {
-		logcabin.Error.Print(err)
-		inputContainers = []string{}
+		return errors.Wrapf(err, "failed to find containers with label %s=%s", dockerops.TypeLabel, strconv.Itoa(dockerops.InputContainer))
 	}
 	for _, ic := range inputContainers {
 		logcabin.Info.Printf("Nuking input container %s", ic)
 		err = dckr.NukeContainer(ic)
 		if err != nil {
-			logcabin.Error.Print(err)
+			return errors.Wrapf(err, "failed to remove container %s", ic)
 		}
 	}
+	return nil
 }
 
 // RemoveDataContainerImages removes the images for the data containers.
-func RemoveDataContainerImages() {
+func RemoveDataContainerImages() error {
 	var err error
 	for _, dc := range job.DataContainers() {
 		logcabin.Info.Printf("Nuking image %s:%s", dc.Name, dc.Tag)
 		err = dckr.NukeImage(dc.Name, dc.Tag)
 		if err != nil {
-			logcabin.Error.Print(err)
+			return errors.Wrapf(err, "failed to remove image %s:%s", dc.Name, dc.Tag)
 		}
 	}
+	return nil
 }
 
 // cleanup encapsulates common job clean up tasks.
 func cleanup(job *model.Job) {
+	var err error
 	logcabin.Info.Printf("Performing aggressive clean up routine...")
-	RemoveInputContainers()
-	RemoveStepContainers()
-	RemoveDataContainers()
-	RemoveVolume(job.InvocationID)
+	if err = RemoveInputContainers(); err != nil {
+		logcabin.Error.Printf("%+v", err)
+	}
+	if err = RemoveStepContainers(); err != nil {
+		logcabin.Error.Printf("%+v", err)
+	}
+	if err = RemoveDataContainers(); err != nil {
+		logcabin.Error.Printf("%+v", err)
+	}
+	if err = RemoveVolume(job.InvocationID); err != nil {
+		logcabin.Error.Printf("%+v", err)
+	}
 }
 
 // Exit handles clean up when road-runner is killed.
 func Exit(exit, finalExit chan messaging.StatusCode) {
+	var err error
 	exitCode := <-exit
 	logcabin.Warning.Printf("Received an exit code of %d, cleaning up", int(exitCode))
 	switch exitCode {
@@ -124,17 +139,31 @@ func Exit(exit, finalExit chan messaging.StatusCode) {
 		//but allow the output containers to run. Yanking the rug out from the
 		//containers should force the Run() function to 'fall through' to any clean
 		//up steps.
-		RemoveDataContainerImages()
-		RemoveInputContainers()
-		RemoveStepContainers()
-		RemoveDataContainers()
-		RemoveVolume(job.InvocationID)
-		RemoveJobContainers(job.InvocationID)
-
+		if err = RemoveDataContainerImages(); err != nil {
+			logcabin.Error.Printf("%+v", err)
+		}
+		if err = RemoveInputContainers(); err != nil {
+			logcabin.Error.Printf("%+v", err)
+		}
+		if err = RemoveStepContainers(); err != nil {
+			logcabin.Error.Printf("%+v", err)
+		}
+		if err = RemoveDataContainers(); err != nil {
+			logcabin.Error.Printf("%+v", err)
+		}
+		if err = RemoveVolume(job.InvocationID); err != nil {
+			logcabin.Error.Printf("%+v", err)
+		}
+		if err = RemoveJobContainers(job.InvocationID); err != nil {
+			logcabin.Error.Printf("%+v", err)
+		}
 	default:
-		RemoveJobContainers(job.InvocationID)
-		RemoveVolume(job.InvocationID)
+		if err = RemoveJobContainers(job.InvocationID); err != nil {
+			logcabin.Error.Printf("%+v", err)
+		}
+		if err = RemoveVolume(job.InvocationID); err != nil {
+			logcabin.Error.Printf("%+v", err)
+		}
 	}
-
 	finalExit <- exitCode
 }
