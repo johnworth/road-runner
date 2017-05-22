a60726df0e84746baeeac9b28ba7080fd9a3031c
John Wregglesworth <wregglej@iplantcollaborative.org>
2017-05-22 07:50:45 -0700

    Formatting changes and comments

--- run.go
+++ run.go
@@ -58,11 +58,17 @@ func (r *JobRunner) Init() error {
 		return err
 	}
 
+	// Copy the docker-compose.yml file into the logs directory. That ensures that
+	// it will end up in the output folder in iRODS. It's very useful for
+	// debugging.
 	err = fs.CopyFile(fs.FS, "docker-compose.yml", path.Join(r.logsDir, "docker-compose.yml"))
 	if err != nil {
 		return err
 	}
 
+	// The de-transfer-trigger.log exists to prevent condor from trying to
+	// transfer every file in the working directory back to the submission
+	// server.
 	transferTrigger, err := os.Create(path.Join(r.logsDir, "de-transfer-trigger.log"))
 	if err != nil {
 		return err
@@ -73,6 +79,8 @@ func (r *JobRunner) Init() error {
 		return err
 	}
 
+	// Put the iplant.cmd file into the logs directory so it ends up back in the
+	// output folder in iRODS. It can be useful for debugging.
 	if _, err = os.Stat("iplant.cmd"); err != nil {
 		if err = os.Rename("iplant.cmd", path.Join(r.logsDir, "iplant.cmd")); err != nil {
 			return err
@@ -85,15 +93,21 @@ func (r *JobRunner) Init() error {
 // DockerLogin will run "docker login" with credentials sent with the job.
 func (r *JobRunner) DockerLogin() error {
 	var err error
+
 	dockerBin := r.cfg.GetString("docker.path")
+
 	// Login so that images can be pulled.
 	var authinfo *authInfo
 	for _, img := range r.job.ContainerImages() {
 		if img.Auth != "" {
+			// The auth information is a base64 encoded JSON string.
 			authinfo, err = parse(img.Auth)
 			if err != nil {
 				return err
 			}
+
+			// This is hokey, but I couldn't find a way to provide auth info to
+			// docker-compose.
 			authCommand := exec.Command(
 				dockerBin,
 				"login",
@@ -103,13 +117,17 @@ func (r *JobRunner) DockerLogin() error {
 				authinfo.Password,
 				parseRepo(img.Name),
 			)
+			// docker login won't run if there isn't a tty.
 			f, err := pty.Start(authCommand)
 			if err != nil {
 				return err
 			}
+
+			// Make sure the output of docker login is logged.
 			go func() {
 				io.Copy(log.Writer(), f)
 			}()
+
 			err = authCommand.Wait()
 			if err != nil {
 				return err
@@ -126,64 +144,78 @@ type JobUpdatePublisher interface {
 }
 
 func (r *JobRunner) createDataContainers() (messaging.StatusCode, error) {
-	var (
-		err error
-	)
+	var err error
 	composePath := r.cfg.GetString("docker-compose.path")
+
 	for index := range r.job.DataContainers() {
 		running(r.client, r.job, fmt.Sprintf("creating data container data_%d", index))
+
 		dataCommand := exec.Command(
 			composePath,
 			"-f",
 			"docker-compose.yml",
 			"up",
-			"--no-color",
+			"--no-color", // Prevents gibberish from getting logged.
 			fmt.Sprintf("data_%d", index),
 		)
 		dataCommand.Env = os.Environ()
 		dataCommand.Stderr = log.Writer()
 		dataCommand.Stdout = log.Writer()
+
 		if err = dataCommand.Run(); err != nil {
 			running(r.client, r.job, fmt.Sprintf("error creating data container data_%d: %s", index, err.Error()))
 			return messaging.StatusDockerCreateFailed, errors.Wrapf(err, "failed to create data container data_%d", index)
 		}
+
 		running(r.client, r.job, fmt.Sprintf("finished creating data container data_%d", index))
 	}
+
 	return messaging.Success, nil
 }
 
 func (r *JobRunner) downloadInputs() (messaging.StatusCode, error) {
-	var (
-		exitCode int64
-	)
+	var exitCode int64
+
+	// The VAULT_ADDR and VAULT_TOKEN environment variables are passed in to
+	// docker-compose so that the values don't appear in the docker-compose.yml
+	// file, which gets sent back to iRODS and is user accessible.
 	env := os.Environ()
 	env = append(env, fmt.Sprintf("VAULT_ADDR=%s", r.cfg.GetString("vault.url")))
 	env = append(env, fmt.Sprintf("VAULT_TOKEN=%s", r.cfg.GetString("vault.token")))
+
 	composePath := r.cfg.GetString("docker-compose.path")
+
 	for index, input := range r.job.Inputs() {
 		running(r.client, r.job, fmt.Sprintf("Downloading %s", input.IRODSPath()))
+
 		stderr, err := os.Create(path.Join(r.logsDir, fmt.Sprintf("logs-stderr-input-%d", index)))
 		if err != nil {
 			log.Error(err)
 		}
 		defer stderr.Close()
+
 		stdout, err := os.Create(path.Join(r.logsDir, fmt.Sprintf("logs-stderr-input-%d", index)))
 		if err != nil {
 			log.Error(err)
 		}
 		defer stdout.Close()
+
 		downloadCommand := exec.Command(composePath, "-f", "docker-compose.yml", "up", "--no-color", fmt.Sprintf("input_%d", index))
 		downloadCommand.Env = env
 		downloadCommand.Stderr = stderr
 		downloadCommand.Stdout = stdout
+
 		if err = downloadCommand.Run(); err != nil {
 			running(r.client, r.job, fmt.Sprintf("error downloading %s: %s", input.IRODSPath(), err.Error()))
 			return messaging.StatusInputFailed, errors.Wrapf(err, "failed to download %s with an exit code of %d", input.IRODSPath(), exitCode)
 		}
+
 		stdout.Close()
 		stderr.Close()
+
 		running(r.client, r.job, fmt.Sprintf("finished downloading %s", input.IRODSPath()))
 	}
+
 	return messaging.Success, nil
 }
 
@@ -214,6 +246,7 @@ func (r *JobRunner) runAllSteps() (messaging.StatusCode, error) {
 				strings.Join(step.Arguments(), " "),
 			),
 		)
+
 		stdout, err := os.Create(path.Join(r.logsDir, fmt.Sprintf("condor-stdout-%d", idx)))
 		if err != nil {
 			log.Error(err)
@@ -227,13 +260,20 @@ func (r *JobRunner) runAllSteps() (messaging.StatusCode, error) {
 		defer stderr.Close()
 
 		composePath := r.cfg.GetString("docker-compose.path")
-		runCommand := exec.Command(composePath, "-f", "docker-compose.yml", "up", "--no-color", fmt.Sprintf("step_%d", idx))
+
+		runCommand := exec.Command(
+			composePath,
+			"-f",
+			"docker-compose.yml",
+			"up",
+			"--no-color",
+			fmt.Sprintf("step_%d", idx),
+		)
 		runCommand.Env = os.Environ()
 		runCommand.Stdout = stdout
 		runCommand.Stderr = stderr
-		err = runCommand.Run()
 
-		if err != nil {
+		if err = runCommand.Run(); err != nil {
 			running(r.client, r.job,
 				fmt.Sprintf(
 					"Error running tool container %s:%s with arguments '%s': %s",
@@ -253,9 +293,11 @@ func (r *JobRunner) runAllSteps() (messaging.StatusCode, error) {
 				strings.Join(step.Arguments(), " "),
 			),
 		)
+
 		stdout.Close()
 		stderr.Close()
 	}
+
 	return messaging.Success, err
 }
 
